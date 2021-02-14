a525e6cf3947b1d34e882be11dc3234b3b7abb40
JackieBinya <jacquelinebinya@gmail.com>
2021-02-15 01:56:59 +0200

    Create global styles
    - Adds fonts to the project directory
    - Reconfigures tailwindconfig extends custom colors and add media queries
    - Creates a global style sheet

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,14 @@
 <template>
- <h1>Dainty</h1>
+  <div>
+    <h1>Dainty</h1>
+    <h2>Dainty</h2>
+    <h4>Dainty</h4>
+    <p>Dainty</p>
+  </div>
 </template>
 
 <script>
 export default {}
 </script>
 
-<style lang="scss" scoped>
-h1{
-  @apply text-textColor;
-}
-</style>
+<style lang="scss" scoped></style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,18 +4,31 @@ module.exports = {
     purgeLayersByDefault: true,
   },
   purge: [
-    './components/**/*.{vue,js}',
-     './layouts/**/*.vue',
-     './pages/**/*.vue',
-     './plugins/**/*.{js,ts}',
-     './nuxt.config.{js,ts}',
+    "./components/**/*.{vue,js}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./nuxt.config.{js,ts}",
   ],
   theme: {
+    screens: {
+      md: { max: "1200px" },
+      // => @media (max-width: 1200px) { ... }
+
+      sm: { max: "480px" },
+      // => @media (max-width: 480px) { ... }
+    },
     extend: {
       colors: {
-        textColor: "var(--text-color)",
+        darkColor: "var(--dark-color)",
+        lightColor: "var(--light-color)",
+        accentPink: "var(--acc-pink-color)",
+        accentPurple: "var(--acc-purple-color)",
+      },
+      fontFamily: {
+        body: "var(--font-body)",
+      },
     },
-  }
   },
   variants: {},
   plugins: [],
