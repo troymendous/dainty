c548581b3728f95d610f619dda5e06d962642627
JackieBinya <jacquelinebinya@gmail.com>
2021-02-16 08:10:00 +0200

    Adds markup and assets for the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,39 @@
 <template>
   <div>
-    <h1>Dainty</h1>
-    <h2>Dainty</h2>
-    <h4>Dainty</h4>
-    <p>Dainty</p>
+    <div class="banner">
+      <div class="banner-animations">
+        <img src="/home/banner/animations/shaps1.png" alt="Animated shapes" />
+        <img src="/home/banner/animations/shaps2.png" alt="Animated shapes" />
+        <img src="/home/banner/animations/shaps3.png" alt="Animated shapes" />
+        <img src="/home/banner/animations/shaps4.png" alt="Animated shapes" />
+        <img src="/home/banner/animations/shaps5.png" alt="Animated shapes" />
+        <img src="/home/banner/animations/shaps6.png" alt="Animated shapes" />
+        <img src="/home/banner/animations/shaps7.png" alt="Animated shapes" />
+      </div>
+    </div>
+
+    <div class="banner-content">
+      <div class="banner-content_text">
+        <h1>
+          Dainty is for your
+          <br />
+          internet app bussiness.
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Vivamus at augue eget arcu dictum. Suscipit tellus
+          mauris a diam. Nam aliquam sem et tortor consequat id porta nibh. Nunc non blandit massa
+          enim.
+        </p>
+        <div>
+          <nuxt-link to="/">Download</nuxt-link>
+          <nuxt-link to="/">Get Started</nuxt-link>
+        </div>
+      </div>
+      <div class="banner-content_showcase">
+        <img src="/home/banner/showcase.png" alt="" />
+      </div>
+    </div>
   </div>
 </template>
 
