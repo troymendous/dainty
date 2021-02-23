2d190ba5b6ca0e1b40303c40d43752dabcbc8f12
JackieBinya <jacquelinebinya@gmail.com>
2021-02-23 08:15:42 +0200

    Add a basic pricing table - no styles👑

--- components/pricing-table.vue
+++ components/pricing-table.vue
@@ -1,9 +1,18 @@
 <template>
   <section>
     <h1>Pricing Table</h1>
-    <pre>
-        {{ pricingTable }}
-    </pre>
+    <table v-for="{ category, id, services } in pricingTable" :key="id">
+      <thead>
+        <th>{{ category }}</th>
+        <th v-for="(heading, index) in headings" :key="index">{{ heading }}</th>
+      </thead>
+      <tbody>
+        <tr v-for="{ service, serviceId, availability } in services" :key="serviceId">
+          <th>{{ service }}</th>
+          <td v-for="(value, index) in availability" :key="index">{{ value }}</td>
+        </tr>
+      </tbody>
+    </table>
   </section>
 </template>
 
@@ -12,6 +21,7 @@ export default {
   data() {
     return {
       pricingTable: [],
+      headings: ["Standard", "Business", "Premium"],
     }
   },
   async fetch() {
