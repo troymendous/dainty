6f1e6a888630f36cccc176cc35c3e85f8ddd0e8e
Valeria <v.ahmetzhanova@gmail.com>
2021-02-25 12:00:50 +0800

    create team-list component

---
+++ components/team/team-list.vue
@@ -0,0 +1,11 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: "TeamList",
+}
+</script>
+
+<style lang="scss"></style>
--- pages/team.vue
+++ pages/team.vue
@@ -1,12 +1,14 @@
 <template>
   <section-header></section-header>
+  <team-list></team-list>
 </template>
 
 <script>
 import SectionHeader from "../components/team/section-header"
+import TeamList from "../components/team/team-list"
 export default {
   name: "Team",
-  components: { SectionHeader },
+  components: { TeamList, SectionHeader },
 }
 </script>
 
