5dad66fd85cd895f1755ae51bac589f5712902e2
Valeria <v.ahmetzhanova@gmail.com>
2021-02-25 11:52:24 +0800

    move team header section to a seperate component

---
+++ components/team/section-header.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <section>
+      <div class="section-header">
+        <div class="section-header_title">
+          <h1>Team</h1>
+        </div>
+      </div>
+      <div id="team-background" class="section-header_background"></div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SectionHeader",
+}
+</script>
+
+<style lang="scss">
+.section-header_title {
+  h1 {
+    font-size: 8rem;
+    font-weight: 600;
+  }
+}
+.section-header {
+  position: relative;
+  display: flex;
+  align-items: flex-end;
+  min-height: 500px;
+  height: auto;
+  padding: 2rem;
+  margin: 2rem;
+
+  @screen md {
+    padding-top: 6rem;
+  }
+
+  @screen sm {
+    padding-top: 3rem;
+  }
+}
+.section-header_background {
+  opacity: 30%;
+  background-position: center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: -1;
+}
+.section-header_background#team-background {
+  background-image: url("../../static/team/team-rs.webp");
+}
+</style>
--- pages/team.vue
+++ pages/team.vue
@@ -1,43 +1,13 @@
 <template>
-  <div>
-    <section class="banner">
-      <div class="banner-inner">
-        <div class="banner-content">
-          <div class="banner-content_title">
-            <h1 class="">Team</h1>
-          </div>
-        </div>
-      </div>
-      <div id="team-background" class="banner_background"></div>
-    </section>
-  </div>
+  <section-header></section-header>
 </template>
 
 <script>
+import SectionHeader from "../components/team/section-header"
 export default {
-  name: "team",
+  name: "Team",
+  components: { SectionHeader },
 }
 </script>
 
-<style lang="scss">
-.banner-content_title {
-  h1 {
-    font-size: 4rem;
-  }
-}
-.banner_background {
-  opacity: 30%;
-  background-position: center center;
-  background-size: cover;
-  background-repeat: no-repeat;
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  z-index: -1;
-}
-.banner_background#team-background {
-  background-image: url("../static/team/team-rs.webp");
-}
-</style>
+<style lang="scss"></style>
