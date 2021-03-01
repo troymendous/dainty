c2bacde3342d341d0b19b3bdde015f9a055fb73b
Valeria <v.ahmetzhanova@gmail.com>
2021-03-01 14:53:36 +0800

    fix close button

--- components/team/team-list.vue
+++ components/team/team-list.vue
@@ -32,70 +32,72 @@
     <div v-if="modalActive" class="team-modal">
       <div class="modal-mask" @click="toggleModal">
         <div class="modal-wrapper">
-          <div class="modal-container" @click.stop>
+          <div class="modal-wrapper-inner">
             <div class="close"></div>
-            <swiper class="swiper" :options="swiperOption">
-              <swiper-slide v-for="{ name, role, link, id } in members" :key="id">
-                <div class="modal-container-text text-scroll">
-                  <h4 class="modal-container_name">
-                    {{ name }}
-                    <a :href="link" target="_blank" class="linkedin">
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="24"
-                        height="24"
-                        viewBox="0 0 24 24"
-                      >
-                        <path
-                          fill="#322F31"
-                          d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
-                        ></path>
-                      </svg>
-                    </a>
-                  </h4>
-                  <h5 class="modal-container_role">{{ role }}</h5>
-                  <h6 class="modal-container_value">VALUE TO THE ROOM</h6>
-                  <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
-                    nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-                    culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit
-                    amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-                    laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-                    reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                    deserunt mollit anim id est laborum."
-                  </p>
-                  <nuxt-link to="/about">
-                    <div class="modal-container_read">Read About Us</div>
-                  </nuxt-link>
-                </div>
-              </swiper-slide>
-            </swiper>
-            <swiper class="swiper" :options="swiperOption">
-              <swiper-slide v-for="{ img, id } in members" :key="id">
-                <div
-                  class="features-carousel-wrapper modal-container_img"
-                  :style="`
+            <div class="modal-container" @click.stop>
+              <swiper class="swiper" :options="swiperOption">
+                <swiper-slide v-for="{ name, role, link, id } in members" :key="id">
+                  <div class="modal-container-text text-scroll">
+                    <h4 class="modal-container_name">
+                      {{ name }}
+                      <a :href="link" target="_blank" class="linkedin">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="24"
+                          height="24"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            fill="#322F31"
+                            d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z"
+                          ></path>
+                        </svg>
+                      </a>
+                    </h4>
+                    <h5 class="modal-container_role">{{ role }}</h5>
+                    <h6 class="modal-container_value">VALUE TO THE ROOM</h6>
+                    <p>
+                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+                      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                      proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem
+                      ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+                      incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+                      nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                      Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+                      eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt
+                      in culpa qui officia deserunt mollit anim id est laborum."
+                    </p>
+                    <nuxt-link to="/about">
+                      <div class="modal-container_read">Read About Us</div>
+                    </nuxt-link>
+                  </div>
+                </swiper-slide>
+              </swiper>
+              <swiper class="swiper" :options="swiperOption">
+                <swiper-slide v-for="{ img, id } in members" :key="id">
+                  <div
+                    class="features-carousel-wrapper modal-container_img"
+                    :style="`
                     background-image: url('${img}');
                   `"
-                >
-                  <div class="container nav">
-                    <div slot="button-prev" class="item modal-button-prev">
-                      <div class="container bg"></div>
-                      <span>Prev</span>
-                    </div>
-                    <div slot="button-next" class="item modal-button-next">
-                      <div class="container bg"></div>
-                      <span>Next</span>
+                  >
+                    <div class="container nav">
+                      <div slot="button-prev" class="item modal-button-prev">
+                        <div class="container bg"></div>
+                        <span>Prev</span>
+                      </div>
+                      <div slot="button-next" class="item modal-button-next">
+                        <div class="container bg"></div>
+                        <span>Next</span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </swiper-slide>
-            </swiper>
+                </swiper-slide>
+              </swiper>
+            </div>
           </div>
         </div>
       </div>
@@ -627,19 +629,35 @@ footer {
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.3);
+    background-color: rgba(0, 0, 0, 0.2);
     display: table;
     transition: opacity 0.3s ease;
   }
   .modal-wrapper {
     display: table-cell;
     vertical-align: middle;
+    .close {
+      position: absolute;
+      width: 30px;
+      height: 30px;
+      top: -5px;
+      right: -5px;
+      background-image: url(../../static/icons/x.svg);
+      background-position: center center;
+      background-size: cover;
+      background-repeat: no-repeat;
+      cursor: pointer;
+      z-index: 10;
+    }
+  }
+  .modal-wrapper-inner {
+    position: relative;
+    width: 1000px;
+    margin: 0px auto;
   }
   .modal-container {
     display: flex;
     position: relative;
-    width: 1000px;
-    margin: 0px auto;
     background-color: #fff;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
     transition: all 0.3s ease;
@@ -659,19 +677,6 @@ footer {
       100% 80px,
       calc(100% - 80px) 0
     );
-    .close {
-      position: absolute;
-      width: 20px;
-      height: 20px;
-      top: 0px;
-      right: 0px;
-      background-image: url(../../static/icons/x.svg);
-      background-position: center center;
-      background-size: cover;
-      background-repeat: no-repeat;
-      cursor: pointer;
-      z-index: 10;
-    }
     .text-scroll {
       max-height: 500px;
       overflow: scroll;
