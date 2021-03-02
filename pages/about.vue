77102105698f610dad07fa27077e26a2a9a96362
Valeria <v.ahmetzhanova@gmail.com>
2021-03-02 11:50:12 +0800

    add values section

--- pages/about.vue
+++ pages/about.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <about-header></about-header>
+    <about-body></about-body>
   </div>
 </template>
 
 <script>
 import AboutHeader from "../components/about/about-header"
+import AboutBody from "../components/about/about-body"
 export default {
   name: "About",
-  components: { AboutHeader },
+  components: { AboutBody, AboutHeader },
 }
 </script>
 
