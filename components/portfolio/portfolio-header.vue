3f4f15236f610b5105d955fb4dafc69a21d5f7f1
Valeria <v.ahmetzhanova@gmail.com>
2021-03-03 18:18:03 +0800

    add portfolio banner text

--- components/portfolio/portfolio-header.vue
+++ components/portfolio/portfolio-header.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="section-header">
+    <div class="portfolio-header-text">
+      <h5>THE LATEST</h5>
+      <h2>Greetings, Hallmark.</h2>
+      <button class="portfolio-header_button">View Case Study</button>
+    </div>
     <div
       class="section-header_background"
       style="background-image: url('/portfolio/hallmark.jpg'); background-position-y: top"
@@ -17,6 +22,22 @@ export default {
 .portfolio {
   .section-header {
     min-height: 600px;
+  }
+  .portfolio-header-text {
+    padding: 50px;
+    margin-bottom: 40px;
+    h5,
+    h2,
+    button {
+      color: white;
+    }
+  }
+  .portfolio-header_button {
+    font-size: 20px;
+    font-weight: 600;
+    padding: 20px 70px;
+    margin: 30px 0;
+    border: 3px solid;
   }
 }
 </style>
