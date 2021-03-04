a579bdca1393777366fcf44a0b5652cfc9448c41
Valeria <v.ahmetzhanova@gmail.com>
2021-03-04 15:27:39 +0800

    create mosaic layout

--- components/portfolio/portfolio-body.vue
+++ components/portfolio/portfolio-body.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>Body</div>
+  <div class="mosaic">
+    <div class="test"></div>
+    <div class="test cols2" style="background-color: aquamarine"></div>
+    <div class="test"></div>
+    <div class="test"></div>
+    <div class="test rows2" style="background-color: rosybrown"></div>
+    <div class="test"></div>
+    <div class="test"></div>
+    <div class="test"></div>
+    <div class="test"></div>
+    <div class="test cols2" style="background-color: aquamarine"></div>
+    <div class="test"></div>
+    <div class="test"></div>
+    <div class="test rows2" style="background-color: rosybrown"></div>
+    <div class="test"></div>
+    <div class="test"></div>
+    <div class="test"></div>
+  </div>
 </template>
 
 <script>
@@ -8,4 +25,28 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style lang="scss">
+.test {
+  min-height: 260px;
+  height: auto;
+  background-color: #b8b8d9;
+}
+.mosaic {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(260px, 1fr));
+  grid-template-rows: auto;
+  grid-gap: 1rem;
+  grid-auto-flow: dense;
+  padding: 1rem;
+}
+.cols2 {
+  @media (min-width: 580px) {
+    grid-column: span 2;
+  }
+}
+.rows2 {
+  @media (min-width: 580px) {
+    grid-row: span 2;
+  }
+}
+</style>
