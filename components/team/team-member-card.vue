a9888f26d0430fcbfeb2f7fd71749722b66cebf5
Valeria <v.ahmetzhanova@gmail.com>
2021-03-09 15:40:02 +0800

    add bio to members

--- components/team/team-member-card.vue
+++ components/team/team-member-card.vue
@@ -21,16 +21,7 @@
       <h5 class="modal-container_role">{{ member.role }}</h5>
       <h6 class="modal-container_value">VALUE TO THE ROOM</h6>
       <p>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-        ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem
-        ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
-        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        {{ member.bio }}
       </p>
       <nuxt-link to="/about" class="modal-link_read">
         <div class="modal-container_read">Read About Us</div>
