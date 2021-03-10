a5aec3b73f890dfdb0eda2cb1ca41637917c2454
JackieBinya <jacquelinebinya@gmail.com>
2021-03-10 11:15:44 +0200

    Adds regex for word card in review cards

--- components/home/reviews.vue
+++ components/home/reviews.vue
@@ -4,7 +4,10 @@
     <div class="reviews-section_inner">
       <div class="review-card" v-for="{ review, name, slug } in reviews" v-bind:key="slug">
         <h5>{{ name }}</h5>
-        <p>{{ review }}</p>
+        <p>{{ review.replace(/(([^\s]+\s\s*){35})(.*)/, "$1…") }}</p>
+        <span class="show-review-btn" v-if="review.match(/(\w+)/g).length > 36" role="button"
+          >Continue reading</span
+        >
       </div>
     </div>
   </section>
@@ -33,17 +36,25 @@ export default {
 
 .reviews-section_inner {
   display: grid;
-  grid-template-columns: repeat(3, 370px);
+  grid-template-columns: repeat(3, 350px);
+  justify-content: space-between;
 }
 
 .review-card {
-  border-radius: 4px;
   padding: 20px;
   margin: 12px auto;
   min-height: 350px !important;
+  border: 1px solid #eaebec;
+  border-radius: 5px;
+  box-shadow: 0 1px 5px 3px #f7f3f3;
 
   p {
     color: black !important;
   }
+}
+
+.show-review-btn {
+  display: inline-block;
+  @apply text-accentPink;
 }
 </style>
