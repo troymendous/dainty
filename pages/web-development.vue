b987acdb4a9a277a7b7783342368849293b82ddb
Valeria <v.ahmetzhanova@gmail.com>
2021-03-11 23:25:53 +0800

    add web-development page

---
+++ pages/web-development.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <section>
+        <div class="about-header">
+          <div class="about-header_title">
+            <h2>
+              How we are working
+              <img class="text-img" src="/about/common_questions.png" alt="" />
+            </h2>
+          </div>
+        </div>
+      </section>
+      <section class="about-body">
+        <div class="text-center section-title">
+          <h2>Web Development</h2>
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "WebDevelopment",
+}
+</script>
+
+<style lang="scss"></style>
