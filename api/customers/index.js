57ae25f18c413e8b43e2e5aa467c760773a54a4f
JackieBinya <jacquelinebinya@gmail.com>
2021-03-15 03:00:46 +0200

    Adds a serverless function to create free trials

---
+++ api/customers/index.js
@@ -0,0 +1,31 @@
+const stripe = require("stripe")(process.env.STRIPE_SK)
+
+const corePlanPriceID = "price_1IRIwiCD5ZUxyIJEU4jaghEZ"
+
+module.exports = async (req, res) => {
+  try {
+    const { id } = await stripe.customers.create({
+      email: req.body.email,
+    })
+
+    await stripe.subscriptions.create({
+      customer: id,
+      items: [
+        {
+          price: corePlanPriceID,
+        },
+      ],
+      trial_period_days: 15,
+    })
+
+    return res.status(200).json({
+      status: 200,
+      message: "Success",
+    })
+  } catch (error) {
+    return res.status(500).json({
+      status: 500,
+      error: err,
+    })
+  }
+}
