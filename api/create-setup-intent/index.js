941edf38c86d85b8ade8762bd8238201ed87833e
JackieBinya <jacquelinebinya@gmail.com>
2021-03-17 17:56:04 +0200

    Adds stripe moodule to the create-setup-intent function

--- api/create-setup-intent/index.js
+++ api/create-setup-intent/index.js
@@ -1,3 +1,5 @@
+const stripe = require("stripe")(process.env.STRIPE_SK)
+
 module.exports = async (req, res) => {
   // Create or use an existing Customer to associate with the SetupIntent.
   // The PaymentMethod will be stored to this Customer for later use.
