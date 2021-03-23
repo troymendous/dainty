1800b366659b17fed8b8e93307442712bc2d0e9f
Valeria <v.ahmetzhanova@gmail.com>
2021-03-23 16:09:31 +0800

    create welcome page

---
+++ pages/welcome.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <section>
+      <div class="about-header">
+        <div class="about-header_title">
+          <h2>Some say clients, we say family</h2>
+        </div>
+      </div>
+    </section>
+    <section class="about-body">
+      <div class="info">
+        <div class="text">
+          Firstly… thank you so much for signing up with us, we’re genuinely so excited to get to
+          know you and your projects.
+        </div>
+        <br />
+        <div class="text">
+          We’ll reach out to you within 24 hours (unless it’s a weekend) with some questionnaires to
+          help get you started.
+        </div>
+        <br />
+        <div class="text">
+          Also, would you like to chat with us live on Slack? If so we’d love to get you involved in
+          our Dainty community, there, you’ll experience faster response times and if we’re online
+          at the same time as you we’ll be able to live chat and talk about any projects on which
+          we’re working on. :-)
+        </div>
+        <br />
+        <div class="text">
+          Alternatively, if you don’t want to wait for us, you can send us an email with your design
+          description here hello@dainty.io and we’ll let you know if we have any questions to fill
+          in any missing gaps.
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+}
+</script>
+
+<style lang="scss"></style>
