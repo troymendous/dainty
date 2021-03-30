6bea63dfab50d1a4d293bf5a50246f679a17096c
JackieBinya <jacquelinebinya@gmail.com>
2021-03-30 11:10:52 +0200

    Uncomment logic for the sendToChannel method

--- mixins/slack-notification-action.js
+++ mixins/slack-notification-action.js
@@ -1,28 +1,20 @@
 export default {
   data() {
     return {
-      message: "Testing from Dainty",
+      message: "Testing from Dainty Client",
     }
   },
   methods: {
-    sendToChannel() {
-      //   const result = await fetch("https://slack.com/api/chat.postMessage", {
-      //     method: "POST",
-      //     headers: {
-      //       "Content-Type": "application/x-www-form-urlencoded",
-      //     },
-      //     data: `token=${token}&channel=${channel}&text=${message}&as_user=false&pretty=1&link_names=true`,
-      //   })
+    async sendToChannel() {
+      const res = await fetch("https://slack.com/api/chat.postMessage", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        data: `token=${process.env.slackNotification}&channel=${process.env.channelNotifs}&text=${this.message}&as_user=false&pretty=1&link_names=true`,
+      })
 
-      //   console.log(result.data)
-
-      //   }).then((response) => {
-      //     console.log("sendToChannelResponse")
-      //     console.log(response.data)
-      //   })
-      //   return result
-
-      console.log("Mixin")
+      console.log(res)
     },
   },
 }
