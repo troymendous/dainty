3a7eba99256a8839551aae104b1be40e2d88e059
JackieBinya <jacquelinebinya@gmail.com>
2021-03-31 10:19:19 +0200

    Adds validation to the str form component

--- components/stripe-checkout/str-form.vue
+++ components/stripe-checkout/str-form.vue
@@ -1,23 +1,23 @@
 <template>
-  <form @submit.prevent="handleSubmit" v-if="!showSetupIntentStep" class="str-form">
-    <h4>Create your account:</h4>
-    <!-- <input type="text" placeholder="Full name*" required v-model="fullname" /> -->
-    <ValidationProvider name="Full name" rules="required|min:3" v-slot="{ errors }">
-      <input type="text" v-model="name" placeholder="Full name*" />
-      <span v-show="errors.length > 0" class="is-invalid">{{ errors[0] }}</span>
-    </ValidationProvider>
+  <ValidationObserver slim v-slot="{ invalid, validate }">
+    <form @submit.prevent="validate().handleSubmit" v-if="!showSetupIntentStep" class="str-form">
+      <h4>Create your account:</h4>
+      <ValidationProvider name="fullname" rules="required|alpha_spaces|min:3" v-slot="{ errors }">
+        <input type="text" v-model="fullname" placeholder="Full name*" />
+        <span v-show="errors.length > 0" class="is-invalid">{{ errors[0] }}</span>
+      </ValidationProvider>
 
-    <ValidationProvider name="Email" rules="required|email" v-slot="{ errors }">
-      <input type="email" v-model="email" placeholder="Work Email*" />
-      <span v-show="errors.length > 0" class="is-invalid">{{ errors[0] }}</span>
-    </ValidationProvider>
+      <ValidationProvider name="email" rules="required|email" v-slot="{ errors }">
+        <input type="email" v-model="email" placeholder="Work Email*" />
+        <span v-show="errors.length > 0" class="is-invalid">{{ errors[0] }}</span>
+      </ValidationProvider>
 
-    <!-- <input type="email" placeholder="Work Email*" required v-model="email" /> -->
-    <str-button :isLoading="isLoading" :disabled="isLoading"> Proceed </str-button>
-    <p class="policy-agreement">
-      By clicking this button, you agree to our Terms, Privacy Policy and Security Policy.
-    </p>
-  </form>
+      <str-button :isLoading="isLoading" :disabled="invalid || isLoading"> Proceed </str-button>
+      <p class="policy-agreement">
+        By clicking this button, you agree to our Terms, Privacy Policy and Security Policy.
+      </p>
+    </form></ValidationObserver
+  >
 </template>
 
 <script>
@@ -50,6 +50,8 @@ export default {
   },
   methods: {
     handleSubmit() {
+      console.log(this.fullname.trim())
+      console.log(this.email)
       console.log("Hello World")
     },
   },
--- plugins/vee-validate.js
+++ plugins/vee-validate.js
@@ -1,10 +1,5 @@
 import Vue from "vue"
-import { ValidationProvider, ValidationObserver, extend } from "vee-validate"
-import { required, min } from "vee-validate/dist/rules"
+import { ValidationProvider, ValidationObserver } from "vee-validate"
 
 Vue.component("ValidationProvider", ValidationProvider)
 Vue.component("ValidationObserver", ValidationObserver)
-
-extend("required", required)
-
-extend("min", min)
