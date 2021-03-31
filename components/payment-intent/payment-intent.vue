8978b6f6b000a9920537cfcdc22e949f5de2e06a
JackieBinya <jacquelinebinya@gmail.com>
2021-03-31 10:47:38 +0200

    Removes redundant code

--- components/payment-intent/payment-intent.vue
+++ components/payment-intent/payment-intent.vue
@@ -34,11 +34,9 @@
 </template>
 
 <script>
-// import loader from "../loader.vue"
 import StrButton from "../stripe-checkout/str-button.vue"
 export default {
   components: {
-    // loader,
     StrButton,
   },
   data() {
@@ -186,31 +184,5 @@ export default {
   .sr-field-error {
     color: red;
   }
-
-  // button {
-  //   border: 2px solid var(--acc-purple-color);
-  //   border-radius: 8px;
-  //   padding: 0.75rem;
-  //   font-weight: 500;
-  //   @apply text-accentPurple;
-  //   margin-top: 2rem;
-  //   min-width: 220px;
-
-  //   svg {
-  //     display: inline-block;
-  //   }
-
-  //   &:hover,
-  //   &:active,
-  //   &:disabled {
-  //     background: var(--acc-pink-color);
-  //     border: 2px solid transparent;
-  //     color: #fff;
-  //   }
-
-  //   &:disabled {
-  //     opacity: 0.8;
-  //   }
-  // }
 }
 </style>
