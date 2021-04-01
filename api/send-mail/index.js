c4cdbf41d99752a9cba8be8c203d8c8c992c4cae
JackieBinya <jacquelinebinya@gmail.com>
2021-04-01 16:39:03 +0200

    Makes createMessage a function

--- api/send-mail/index.js
+++ api/send-mail/index.js
@@ -4,13 +4,15 @@ const mailgun = new Mailgun(formData)
 
 const mg = mailgun.client({ username: "api", key: process.env.MAILGUN_API_KEY })
 
-const createMessage = mg.messages.create("dainty.io", {
-  from: "Excited User <no-reply@dainty.io>",
-  to: ["jackie@dainty.io"],
-  subject: "Hello",
-  text: "Testing some Mailgun awesomness!",
-  html: "<h1>Testing some Mailgun awesomness!</h1>",
-})
+const createMessage = function () {
+  mg.messages.create("dainty.io", {
+    from: "Excited User <no-reply@dainty.io>",
+    to: ["jackie@dainty.io"],
+    subject: "Hello",
+    text: "Testing some Mailgun awesomness!",
+    html: "<h1>Testing some Mailgun awesomness!</h1>",
+  })
+}
 // .then(msg => console.log(msg)) // logs response data
 // .catch(err => console.log(err));
 
