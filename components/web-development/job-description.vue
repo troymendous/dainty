b9163a9204e8634a4da6d338b44c15657db4c45e
JackieBinya <jacquelinebinya@gmail.com>
2021-04-20 23:32:16 +0200

    Adds basic layout for the job description section

---
+++ components/web-development/job-description.vue
@@ -0,0 +1,100 @@
+<template>
+  <section>
+    <div class="icon-container">
+      <div class="icon-container_inner">
+        <div class="icon-col">
+          <span class="sprite-image-front sprite-hand"></span>
+          <div class="heading">Never lift a finger</div>
+          <p class="text">Because time is better spent not wrestling WordPress</p>
+        </div>
+        <div class="icon-col">
+          <span class="sprite-image-front sprite-bug"></span>
+          <div class="heading">A bug free site</div>
+          <p class="text">Because broken sites mean lost customers</p>
+        </div>
+        <div class="icon-col">
+          <span class="sprite-image-front sprite-cal"></span>
+          <div class="heading">24/7/365</div>
+          <p class="text">Because problems happen at the worst possible time</p>
+        </div>
+      </div>
+    </div>
+    <div class="image-container">
+      <img src="/web-development/bg-dot.png" alt="" />
+    </div>
+    <p class="desc">
+      Our developers are available 24/7 to fix any small jobs, on the same day, from $79 /month.
+    </p>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.icon-container {
+  margin: 0 auto;
+  width: 92%;
+  max-width: 960px;
+}
+.icon-container_inner {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+}
+.sprite-image-front {
+  background: url(../../static/web-development/sprite-front.png) no-repeat transparent;
+}
+.sprite-hand {
+  background-position: -422px 0px;
+  background-size: auto 72px;
+  display: inline-block;
+  width: 88px;
+  height: 72px;
+}
+
+.sprite-bug {
+  background-position: -98px 0px;
+  background-size: auto 72px;
+  display: inline-block;
+  width: 88px;
+  height: 72px;
+}
+
+.sprite-cal {
+  background-position: -186px 0px;
+  background-size: auto 72px;
+  display: inline-block;
+  width: 88px;
+  height: 72px;
+}
+
+.icon-col {
+  text-align: center;
+
+  .text {
+    max-width: 600px;
+  }
+}
+
+.image-container {
+  margin: 0 auto;
+  width: 92%;
+  max-width: 960px;
+  display: flex;
+  justify-content: center;
+  img {
+    max-width: 607px;
+    width: 70%;
+    height: auto;
+  }
+}
+
+.desc {
+  max-width: 900px;
+  margin: 0 auto;
+  padding-bottom: 60px;
+  border-bottom: 1px solid grey;
+  text-align: center;
+}
+</style>
--- pages/webdevelopment-new.vue
+++ pages/webdevelopment-new.vue
@@ -1,12 +1,18 @@
 <template>
-  <header-section />
+  <div>
+    <header-section />
+    <job-description />
+  </div>
 </template>
 
 <script>
 import HeaderSection from "../components/web-development/header-section"
+import JobDescription from "../components/web-development/job-description"
+
 export default {
   components: {
     HeaderSection,
+    JobDescription,
   },
 }
 </script>
