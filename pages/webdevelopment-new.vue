1b02b191f3ab20a3e492f2ff050aa9e8f7feb6d4
JackieBinya <jacquelinebinya@gmail.com>
2021-04-22 12:45:12 +0200

    Adds a basic leaflet map

--- pages/webdevelopment-new.vue
+++ pages/webdevelopment-new.vue
@@ -4,6 +4,14 @@
     <job-description />
     <services-offered />
     <how-it-works />
+    <div id="map-wrap" style="height: 100vh">
+      <no-ssr>
+        <l-map :zoom="13" :center="[55.9464418, 8.1277591]">
+          <l-tile-layer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"></l-tile-layer>
+          <l-marker :lat-lng="[55.9464418, 8.1277591]"></l-marker>
+        </l-map>
+      </no-ssr>
+    </div>
   </div>
 </template>
 
