6d2502e0c792d87cea51aaff709f3f602f3bb703
JackieBinya <jacquelinebinya@gmail.com>
2021-05-09 17:18:56 +0200

    Restructure zapier-docs mixin

--- mixins/zapier-docs.js
+++ mixins/zapier-docs.js
@@ -1,4 +1,8 @@
 export default {
+  mounted() {
+    console.log(new Date())
+    console.log({ time: this.createdDateTime })
+  },
   methods: {
     async appendUsersToGoogleDocs(plan) {
       const result = await fetch("/api/send-user-to-google-doc", {
@@ -10,11 +14,16 @@ export default {
           email: this.email.trim(),
           name: this.fullname.trim(),
           plan,
-          createdDateTime: new Date().toUTCString(),
+          createdDateTime: this.createdDateTime,
         }),
       })
 
       console.log(result)
+    },
+  },
+  computed: {
+    createdDateTime() {
+      return new Date()
     },
   },
 }
