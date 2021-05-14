6964a89ed29dde79009d5a3105bbc809cd9c72b8
JackieBinya <jacquelinebinya@gmail.com>
2021-05-14 02:25:33 +0200

    Adds test page

---
+++ pages/test.vue
@@ -0,0 +1,101 @@
+<template>
+  <div class="free-trial">
+    <h2>Checkout</h2>
+    <div class="free-trial_inner">
+      <div class="free-trial_content-wrapper">
+        <div class="free-trial_content">
+          <str-form
+            :isLoading="isLoading"
+            :showNextStep="showPaymentIntentStep"
+            v-on:showStrCheckout="toggleShowPaymentIntentStep"
+          />
+          <paymentIntent
+            v-show="showPaymentIntentStep"
+            v-on:closePaymentIntent="closePaymentIntentStep"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import paymentIntent from "../components/payment-intent/payment-intent.vue"
+import StrForm from "../components/stripe-checkout/str-form"
+
+export default {
+  components: {
+    paymentIntent,
+    StrForm,
+  },
+  data() {
+    return {
+      showPaymentIntentStep: false,
+      isLoading: false,
+      slug: "",
+    }
+  },
+  mounted() {
+    this.slug = this.$route.params.slug
+  },
+  computed: {
+    email: {
+      get() {
+        return this.$store.state.email
+      },
+      set(value) {
+        this.$store.commit("updateEmail", value)
+      },
+    },
+    fullname: {
+      get() {
+        return this.$store.state.fullname
+      },
+      set(value) {
+        this.$store.commit("updateFullname", value)
+      },
+    },
+  },
+  methods: {
+    closePaymentIntentStep() {
+      this.showPaymentIntentStep = false
+    },
+    toggleShowPaymentIntentStep() {
+      this.showPaymentIntentStep = true
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.free-trial {
+  width: 100%;
+  max-width: 1240px;
+  margin: 0 auto;
+  padding-top: 9rem;
+
+  h2 {
+    text-align: center;
+  }
+}
+
+.free-trial_inner {
+  margin-top: 3rem;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1.5rem;
+
+  @screen md {
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+
+.free-trial_content-wrapper {
+  border-top: 1px solid #d5d5d5;
+  padding: 30px 60px 60px;
+
+  @screen sm {
+    padding: 20px 30px;
+  }
+}
+</style>
