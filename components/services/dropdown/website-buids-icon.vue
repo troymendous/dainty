14bf002aebe1df5119570c6a18daaa0e730b48a5
JackieBinya <jacquelinebinya@gmail.com>
2021-06-01 22:41:56 +0200

    Adds a dropdown menu in the desktop screen

---
+++ components/services/dropdown/website-buids-icon.vue
@@ -0,0 +1,54 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48">
+    <g id="_1" data-name="1" transform="translate(-3733 -237)">
+      <circle
+        id="Ellipse_123"
+        data-name="Ellipse 123"
+        cx="24"
+        cy="24"
+        r="24"
+        transform="translate(3733 237)"
+        fill="#f8f6ff"
+      />
+      <g id="coding" transform="translate(3742 196.522)">
+        <g id="Group_393" data-name="Group 393" transform="translate(20.748 55.952)">
+          <g id="Group_392" data-name="Group 392">
+            <path
+              id="Path_5483"
+              data-name="Path 5483"
+              d="M364.142,119.11l-6.987-7.394a1.132,1.132,0,1,0-1.648,1.553l6.253,6.617-6.253,6.617a1.133,1.133,0,0,0,1.648,1.554l6.987-7.394A1.131,1.131,0,0,0,364.142,119.11Z"
+              transform="translate(-355.199 -111.361)"
+              fill="#5b39c9"
+            />
+          </g>
+        </g>
+        <g id="Group_395" data-name="Group 395" transform="translate(0.001 55.952)">
+          <g id="Group_394" data-name="Group 394" transform="translate(0 0)">
+            <path
+              id="Path_5484"
+              data-name="Path 5484"
+              d="M8.944,126.5l-6.252-6.617,6.252-6.617A1.132,1.132,0,0,0,7.3,111.715L.31,119.108a1.131,1.131,0,0,0,0,1.554L7.3,128.056A1.133,1.133,0,0,0,8.944,126.5Z"
+              transform="translate(-0.001 -111.36)"
+              fill="#5b39c9"
+            />
+          </g>
+        </g>
+        <g id="Group_397" data-name="Group 397" transform="translate(12.206 52.478)">
+          <g id="Group_396" data-name="Group 396" transform="translate(0 0)">
+            <path
+              id="Path_5485"
+              data-name="Path 5485"
+              d="M213.264,52.491a1.134,1.134,0,0,0-1.291.948L208.65,75.175a1.132,1.132,0,0,0,.949,1.29,1.169,1.169,0,0,0,.173.013,1.133,1.133,0,0,0,1.119-.961l3.323-21.736A1.132,1.132,0,0,0,213.264,52.491Z"
+              transform="translate(-208.636 -52.478)"
+              fill="#5b39c9"
+            />
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script>
+export default {}
+</script>
