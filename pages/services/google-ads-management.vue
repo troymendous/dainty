f58e027c303f8b777f46b6f17efae5d171164548
JackieBinya <jacquelinebinya@gmail.com>
2021-06-01 04:25:11 +0200

    Adds services

---
+++ pages/services/google-ads-management.vue
@@ -0,0 +1,12 @@
+<template>
+  <coming-soon />
+</template>
+
+<script>
+import comingSoon from "../../components/utility-components/coming-soon.vue"
+export default {
+  components: { comingSoon },
+}
+</script>
+
+<style lang="scss" scoped></style>
