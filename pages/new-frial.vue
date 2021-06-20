a91122b3cba1d9e3befad686dd38e030bc8a914e
JackieBinya <jacquelinebinya@gmail.com>
2021-06-20 10:36:50 +0200

    Creates basic functionality of the onboarding checkboxes features

---
+++ pages/new-frial.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <design-type-step />
+    <business-type-step />
+  </div>
+</template>
+
+<script>
+import BusinessTypeStep from "../components/onboarding/business-type-step.vue"
+import DesignTypeStep from "../components/onboarding/design-type-step.vue"
+
+export default {
+  components: { DesignTypeStep, BusinessTypeStep },
+}
+</script>
+
+<style lang="scss" scoped></style>
--- store/index.js
+++ store/index.js
@@ -4,6 +4,45 @@ export const state = () => ({
   email: "",
   fullname: "",
   customer: "",
+  designTypes: [
+    {
+      type: "Banner Design",
+      checked: true,
+      iconPath: "/onboarding/banner-design_icon.svg",
+    },
+    {
+      type: "Logo Design",
+      checked: true,
+      iconPath: "/onboarding/logo-design_icon.svg",
+    },
+    {
+      type: "Social media Post",
+      checked: true,
+      iconPath: "/onboarding/social-media_icon.svg",
+    },
+    {
+      type: "Other design",
+      checked: true,
+      iconPath: "/onboarding/other-design_icon.svg",
+    },
+  ],
+  businessTypes: [
+    {
+      type: "Personal",
+      checked: true,
+      iconPath: "/onboarding/personal_icon.svg",
+    },
+    {
+      type: "Business",
+      checked: true,
+      iconPath: "/onboarding/business_icon.svg",
+    },
+    {
+      type: "Other",
+      checked: true,
+      iconPath: "/onboarding/other_icon.svg",
+    },
+  ],
 })
 
 export const mutations = {
