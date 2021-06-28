818362cebd9eda83d9de3c2ac6ed6cee7b6e11ab
srijan-katuwal <srijan.katuwal@gmail.com>
2021-06-28 18:11:23 +0545

    Onboarding design / web development page

---
+++ components/web-development/web-development-card.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="development-card">
+    <div class="card-top">
+      <img class="card-image" :src="image" />
+      <img
+        class="checked-image"
+        :src="checked ? '/web-development/checked.svg' : '/web-development/unchecked.svg'"
+      />
+    </div>
+    <h5>{{ text }}</h5>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    image: {
+      type: String,
+      default: () => "",
+    },
+    checked: {
+      type: Boolean,
+      default: () => false,
+    },
+    text: {
+      type: String,
+      default: () => "",
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.development-card {
+  box-shadow: 0px 3px 16px #77777729;
+  border-radius: 8px;
+  padding: 24px;
+  background: white;
+  .card-top {
+    display: flex;
+    justify-content: space-between;
+    align-items: start;
+    .card-image {
+      @screen sm {
+        width: 36px;
+        height: 36px;
+      }
+    }
+    .checked-image {
+      width: 24px;
+      width: 24px;
+      @screen sm {
+        width: 16px;
+        height: 16px;
+      }
+    }
+  }
+  h5 {
+    margin: 17px 0 0;
+  }
+}
+</style>
---
+++ components/web-development/web-development-header.vue
@@ -0,0 +1,74 @@
+<template>
+  <section class="development-header">
+    <img src="/web-development/header.png" />
+    <h3>What was the design you are after?</h3>
+    <div class="h3-bottom-border"></div>
+    <div class="development-cards">
+      <web-development-card v-for="(card, i) in cards" :key="i" v-bind="card" />
+    </div>
+  </section>
+</template>
+
+<script>
+import WebDevelopmentCard from "./web-development-card.vue"
+export default {
+  components: {
+    WebDevelopmentCard,
+  },
+  data: () => ({
+    cards: [
+      { image: "/web-development/1.svg", checked: false, text: "Banner Design" },
+      { image: "/web-development/2.svg", checked: true, text: "Logo Design" },
+      { image: "/web-development/3.svg", checked: false, text: "Social Media Post" },
+      { image: "/web-development/4.svg", checked: false, text: "Other Design" },
+    ],
+  }),
+}
+</script>
+
+<style lang="scss" scoped>
+.development-header {
+  max-width: 1152px;
+  margin: auto;
+  padding: 8rem 0 0;
+  @screen md {
+    padding: 6rem 2rem;
+  }
+  @screen sm {
+    padding: 5rem 1rem;
+  }
+  img {
+    width: 260px;
+    margin: auto;
+    @screen sm {
+      width: 180px;
+    }
+  }
+  h3 {
+    text-align: center;
+    @screen sm {
+      margin: 30px 3rem 0;
+    }
+  }
+  .h3-bottom-border {
+    border: 1px solid var(--acc-purple-color);
+    width: 40px;
+    margin: 12px auto 36px;
+    @screen sm {
+      margin: 12px auto 30px;
+    }
+  }
+  .development-cards {
+    @apply grid;
+    @apply grid-cols-4;
+    width: 100%;
+    gap: 25px;
+    @screen md {
+      @apply grid-cols-2;
+    }
+    @screen sm {
+      gap: 10px;
+    }
+  }
+}
+</style>
