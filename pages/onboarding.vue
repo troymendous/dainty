ba2a0d12f7d1d1b9cd47b0f5b888f75630b0a401
Srijan Katuwal <srijan.kkatuwal@gmail.com>
2021-07-09 10:19:49 +0545

    Restore web-developemnt page

---
+++ pages/onboarding.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="web-development-section">
+    <web-development-header />
+  </div>
+</template>
+
+<script>
+import WebDevelopmentHeader from "../components/web-development/web-development-header"
+export default {
+  name: "WebDevelopment",
+  components: {
+    WebDevelopmentHeader,
+  },
+}
+</script>
+
+<style lang="scss">
+.web-development-section {
+  width: 100%;
+  background: transparent linear-gradient(181deg, #f3fbff 0%, #ffffff00 100%) 0% 0% no-repeat
+    padding-box;
+}
+</style>
