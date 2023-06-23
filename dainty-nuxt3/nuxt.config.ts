384276c2f0d1fd8edee3a34994a8d89d181f9eb6
baseerx <bsronnet@gmail.com>
2023-06-23 10:35:27 +0500

    dainty nuxt 3 version

---
+++ dainty-nuxt3/nuxt.config.ts
@@ -0,0 +1,13 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts","nuxt-icon"],
+  googleFonts: {
+    families: {
+      Poppins: true,
+      "Poly Rounded": true,
+
+      // Add other font families here
+    },
+  },
+});
---
+++ dainty-nuxt3/pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+
+  <div class="text-black font-poppins">
+    container for pages looking forward for its design as well
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+.font-poppins {
+  font-family: "Poppins";
+}
+</style>
